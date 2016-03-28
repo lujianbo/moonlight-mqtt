7ecf5c17794ace3501b78b52a3e313e4ec53cd05
jianbo <387852424@qq.com>
2016-03-28 14:02:20 +0800

    init

--- src/main/java/io/github/lujianbo/mqtt/websocket/WebSocketTransportHandler.java
+++ src/main/java/io/github/lujianbo/mqtt/websocket/WebSocketTransportHandler.java
@@ -1,9 +1,40 @@
 package io.github.lujianbo.mqtt.websocket;
 
-/**
- * Created by jianbo on 2016/3/25.
- */
-public class WebSocketTransportHandler {
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Locale;
 
+public class WebSocketTransportHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
+
+    private final WebSocketServerHandshaker handshaker;
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketTransportHandler.class);
+
+    public WebSocketTransportHandler(WebSocketServerHandshaker handshaker) {
+        this.handshaker = handshaker;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+        if (frame instanceof TextWebSocketFrame) {
+            //TextWebSocketFrame 已经被禁止
+            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+
+        //Mqtt只能通过BinaryWebSocket来进行传输
+        if (frame instanceof BinaryWebSocketFrame) {
+            //将数据传输到下一个handler
+            ctx.fireChannelRead(frame.content());
+            return;
+        }
+    }
 }
