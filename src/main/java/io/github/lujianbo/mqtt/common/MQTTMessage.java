a9a65a1967cd65fdbf48d594489fe113f71a50f9
lujianbo <387852424@qq.com>
2016-04-18 11:47:35 +0800

    fix

--- src/main/java/io/github/lujianbo/mqtt/common/MQTTMessage.java
+++ src/main/java/io/github/lujianbo/mqtt/common/MQTTMessage.java
@@ -1,7 +1,25 @@
 package io.github.lujianbo.mqtt.common;
 
 /**
- * Created by lujianbo on 2016/4/18.
+ * 一条消息的抽象
  */
 public class MQTTMessage {
+
+    /**
+     * 发送者
+     * */
+    public String sender;
+
+    /**
+     * 目标topic
+     * */
+    public String  topic;
+
+    /**
+     * 消息内容
+     * */
+    public byte[] playLoad;
+
+
+
 }
